67be86d8680b238a2bcf5d8909965ce2221f3f4d
cecemel <sebastien.gotfroi@gmail.com>
2017-03-11 08:37:36 -0500

    premier mock !!!!! Badass

--- src/app/service/access-cible.service.ts
+++ src/app/service/access-cible.service.ts
@@ -1,8 +1,23 @@
 import {Injectable} from "@angular/core";
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
+import { Result, Bus } from '../models/result';
 
 @Injectable()
 export class AccesCibleService {
+  getMock(): Result{
+    let result: Result = new Result();
 
+    let position: {lat: string, lng: string}[];
+    position.push({'lat':'7.464655', 'lng': '42.5655'});
+    position.push({'lat':'7.464789', 'lng': '42.5681'});
+    position.push({'lat':'7.464962', 'lng': '42.5560'});
+    position.push({'lat':'7.464800', 'lng': '42.5792'});
+    position.push({'lat':'7.464457', 'lng': '42.5450'});
+
+    result.bus = new Bus();
+    result.bus.positionsPol = position;
+
+    return result;
+  }
 }

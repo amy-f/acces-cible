b9dfd02c56830edcd8b7f4795fb4f3b238f4aead
cecemel <sebastien.gotfroi@gmail.com>
2017-03-10 23:13:43 -0500

    add service

--- src/app/the-page/the-page.component.ts
+++ src/app/the-page/the-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AccesCibleService } from '../service/access-cible.service';
 
 @Component({
   selector: 'app-the-page',
   templateUrl: './the-page.component.html',
-  styleUrls: ['./the-page.component.css']
+  styleUrls: ['./the-page.component.css'],
+  providers: [AccesCibleService]
 })
 export class ThePageComponent implements OnInit {
 

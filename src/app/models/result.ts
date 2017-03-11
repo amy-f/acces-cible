0bc28e7fd7914c646fdbf77fcace81a0470ce3a6
cecemel <sebastien.gotfroi@gmail.com>
2017-03-11 08:21:00 -0500

    ajout du model

---
+++ src/app/models/result.ts
@@ -0,0 +1,35 @@
+export class result {
+    bus: {
+      positionsPol : {
+        lat: string;
+        long: string;
+      }[],
+      ligne: {
+        stop: {
+          lat: string;
+          long: string;
+        },
+        distance: number;
+      }[]
+    };
+    metro: {
+      positionsPol : {
+        lat: string;
+        long: string;
+      }[],
+      ligne: {
+        stop: {
+          lat: string;
+          long: string;
+        },
+        distance: number;
+      }[]
+    };
+    velo: {
+      start: {
+        lat: string;
+        long: string;
+      },
+      distance: number;
+    }
+}

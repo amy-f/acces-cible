297564fb249ea22b8a43b2971c1407f10e8ea2f9
cecemel <sebastien.gotfroi@gmail.com>
2017-03-11 09:36:52 -0500

    rajout service app module

--- src/app/models/result.ts
+++ src/app/models/result.ts
@@ -1,35 +1,32 @@
-export class result {
-    bus: {
-      positionsPol : {
-        lat: string;
-        long: string;
-      }[],
-      ligne: {
-        stop: {
-          lat: string;
-          long: string;
-        },
-        distance: number;
-      }[]
-    };
-    metro: {
-      positionsPol : {
-        lat: string;
-        long: string;
-      }[],
-      ligne: {
-        stop: {
-          lat: string;
-          long: string;
-        },
-        distance: number;
-      }[]
-    };
+export class Result {
+    bus: TransportCom;
+    metro: TransportCom;
     velo: {
       start: {
         lat: string;
-        long: string;
+        lng: string;
       },
       distance: number;
     }
 }
+
+export class TransportCom {
+  positionsPol : {
+    lat: string;
+    lng: string;
+  }[];
+  ligne: {
+    stop: {
+      lat: string;
+      lng: string;
+    },
+    nom: string;
+    distance: number;
+  }[]
+}
+
+export class Velo {
+
+}
+
+

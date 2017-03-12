e5a775a417d43153d027abc1de5dce5a42e6120b
cecemel <sebastien.gotfroi@gmail.com>
2017-03-11 23:35:17 -0500

    'mm'

--- src/app/page-resultat/page-resultat.component.ts
+++ src/app/page-resultat/page-resultat.component.ts
@@ -74,6 +74,7 @@ export class PageResultatComponent implements OnInit {
 
   getPaths() {
     let result = this.accesCibleService.getMock();
+    this.compute(result);
     // let result = this.accesCibleService.getHttp(this.address.latitude, this.address.longitude).subscribe(
     //   res => this.compute(res)
     // );
